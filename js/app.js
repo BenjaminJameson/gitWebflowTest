1a591d89b4ac000e72210035bc6f3bc33789191d
Ben Jameson <benjamesonn@gmail.com>
2021-03-08 13:52:47 +1100

    added js button

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+function runbutton1(params) {
+    
+    let existNum = document.getElementById("output-text").innerHTML;
+
+    let newNum = existNum+1;
+    document.getElementById("output-text").innerHTML = newNum;
+}
